1ae16fab27b894b381707d44bf3e9b045341ff20
shailahasib <shailahasib1016@gmail.com>
2020-11-09 20:23:25 -0500

    errors fixed

--- Generic/src/main/java/common/WebAPI.java
+++ Generic/src/main/java/common/WebAPI.java
@@ -125,7 +125,7 @@ public class WebAPI {
     @Parameters({"useCloudEnv", "cloudEnvName", "OS", "os_version", "browserName", "browserVersion", "url"})
     @BeforeMethod
     public void setUp(@Optional("false") boolean useCloudEnv, @Optional("sauceLabs") String cloudEnvName, @Optional("OS X") String OS, @Optional("10") String os_version, @Optional("chrome") String browserName,
-                      @Optional("86") String browserVersion, @Optional("https://walmart.com") String url) throws IOException {
+                      @Optional("86") String browserVersion, @Optional("https://google.com") String url) throws IOException {
         // Platform: Local Machine/ cloud machine
         if (useCloudEnv == true) {
             if (cloudEnvName.equalsIgnoreCase("browserStack")) {
