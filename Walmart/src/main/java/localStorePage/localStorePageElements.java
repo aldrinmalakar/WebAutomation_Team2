0ddea98a1da55cf26e8a89cede00fae8d629362a
aldrinmalakar <aldrinmalakar@gmail.com>
2020-11-10 01:32:34 -0500

    A Little bit more than 5minutes ago

--- Walmart/src/main/java/localStorePage/localStorePageElements.java
+++ Walmart/src/main/java/localStorePage/localStorePageElements.java
@@ -1,4 +1,14 @@
 package localStorePage;
 
 public class localStorePageElements {
+    //Before Method Variables
+    public static final String localStoreURL = "https://www.walmart.com/store/";
+
+    public static final String makeThisYourStore = "//button[contains(text(),'Make this your store')]";
+    public static final String getMakeThisYourStore = "store-number";
+    public static final String findAnotherStore = "//button[normalize-space()='Find another store']";
+    public static final String getFindAnotherStore = "//p[normalize-space()='Nearby Stores']";
+    public static final String findDirection = "//div[@class='directions-and-phone-parts-cell-right']//span[contains(text(),'Get directions')]";
+    public static final String callTheStore = "//div[@class='directions-and-phone-parts-cell-right']//div[@id='store-phone']";
+    public static final String
 }
