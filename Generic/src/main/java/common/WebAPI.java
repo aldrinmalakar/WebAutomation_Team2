36a3cb8d94b314a527428a5c9ed812feb36dee39
sadiaiguerguit <sidialiifri82@gmail.com>
2020-12-01 11:30:39 -0500

    Automation

--- Generic/src/main/java/common/WebAPI.java
+++ Generic/src/main/java/common/WebAPI.java
@@ -125,7 +125,7 @@ public class WebAPI {
     @Parameters({"useCloudEnv", "cloudEnvName", "OS", "os_version", "browserName", "browserVersion", "url"})
     @BeforeMethod
     public void setUp(@Optional("false") boolean useCloudEnv, @Optional("sauceLabs") String cloudEnvName, @Optional("windows") String OS, @Optional("10") String os_version, @Optional("chrome") String browserName,
-                      @Optional("86") String browserVersion, @Optional("https://www.google.com/") String url) throws IOException {
+                      @Optional("86") String browserVersion, @Optional("https://www.CNN.com/") String url) throws IOException {
         // Platform: Local Machine/ cloud machine
         if (useCloudEnv == true) {
             if (cloudEnvName.equalsIgnoreCase("browserStack")) {
